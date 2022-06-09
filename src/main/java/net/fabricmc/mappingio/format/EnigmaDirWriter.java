5c1ae32d3fd97e08478f6a9c1a7f41c20f820be6
Julian Burner <burnerjulian@gmail.com>
2022-06-09 04:53:58 +0200

    Add support for writing single Enigma files

--- src/main/java/net/fabricmc/mappingio/format/EnigmaDirWriter.java
+++ src/main/java/net/fabricmc/mappingio/format/EnigmaDirWriter.java
@@ -18,7 +18,6 @@ package net.fabricmc.mappingio.format;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.Writer;
 import java.nio.file.DirectoryNotEmptyException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -27,23 +26,20 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
-import java.util.EnumSet;
 import java.util.List;
-import java.util.Set;
 
 import net.fabricmc.mappingio.MappedElementKind;
-import net.fabricmc.mappingio.MappingFlag;
-import net.fabricmc.mappingio.MappingWriter;
 
-public final class EnigmaDirWriter implements MappingWriter {
+public final class EnigmaDirWriter extends EnigmaWriterBase {
 	public EnigmaDirWriter(Path dir, boolean deleteExistingFiles) throws IOException {
+		super(null);
 		this.dir = dir.toAbsolutePath().normalize();
 
 		if (deleteExistingFiles && Files.exists(dir)) {
 			Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
 				@Override
 				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-					if (file.getFileName().toString().endsWith(".mapping")) {
+					if (file.getFileName().toString().endsWith("." + MappingFormat.ENIGMA.fileExt)) {
 						Files.delete(file);
 					}
 
@@ -69,76 +65,7 @@ public final class EnigmaDirWriter implements MappingWriter {
 		if (writer != null) {
 			writer.close();
 			writer = null;
-			writerClass = null;
-		}
-	}
-
-	@Override
-	public Set<MappingFlag> getFlags() {
-		return flags;
-	}
-
-	@Override
-	public void visitNamespaces(String srcNamespace, List<String> dstNamespaces) { }
-
-	@Override
-	public boolean visitClass(String srcName) {
-		srcClassName = srcName;
-		return true;
-	}
-
-	@Override
-	public boolean visitField(String srcName, String srcDesc) throws IOException {
-		writeIndent(0);
-		writer.write("FIELD ");
-		writer.write(srcName);
-
-		desc = srcDesc;
-
-		return true;
-	}
-
-	@Override
-	public boolean visitMethod(String srcName, String srcDesc) throws IOException {
-		writeIndent(0);
-		writer.write("METHOD ");
-		writer.write(srcName);
-
-		desc = srcDesc;
-
-		return true;
-	}
-
-	@Override
-	public boolean visitMethodArg(int argPosition, int lvIndex, String srcName) throws IOException {
-		writeIndent(1);
-		writer.write("ARG ");
-		writer.write(Integer.toString(lvIndex));
-
-		return true;
-	}
-
-	@Override
-	public boolean visitMethodVar(int lvtRowIndex, int lvIndex, int startOpIdx, String srcName) {
-		return false;
-	}
-
-	@Override
-	public boolean visitEnd() throws IOException {
-		close();
-
-		return true;
-	}
-
-	@Override
-	public void visitDstName(MappedElementKind targetKind, int namespace, String name) throws IOException {
-		if (namespace != 0) return;
-
-		if (targetKind == MappedElementKind.CLASS) {
-			dstName = name;
-		} else {
-			writer.write(' ');
-			writer.write(name);
+			currentClass = null;
 		}
 	}
 
@@ -147,21 +74,21 @@ public final class EnigmaDirWriter implements MappingWriter {
 		if (targetKind == MappedElementKind.CLASS) {
 			String name = dstName != null ? dstName : srcClassName;
 
-			if (writerClass == null
-					|| !name.startsWith(writerClass)
-					|| name.length() > writerClass.length() && name.charAt(writerClass.length()) != '$') {
+			if (currentClass == null
+					|| !name.startsWith(currentClass)
+					|| name.length() > currentClass.length() && name.charAt(currentClass.length()) != '$') {
 				int pos = getNextOuterEnd(name, 0);
 				if (pos >= 0) name = name.substring(0, pos);
 
 				// currentClass is not an outer class of srcName (or the same)
-				Path file = dir.resolve(name+".mapping").normalize();
-				if (!file.startsWith(dir)) throw new RuntimeException("invalid name: "+name);
+				Path file = dir.resolve(name + "." + MappingFormat.ENIGMA.fileExt).normalize();
+				if (!file.startsWith(dir)) throw new RuntimeException("invalid name: " + name);
 
 				if (writer != null) {
 					writer.close();
 				}
 
-				writerClass = name;
+				currentClass = name;
 
 				if (Files.exists(file)) {
 					// initialize writtenClass with last CLASS entry
@@ -193,61 +120,16 @@ public final class EnigmaDirWriter implements MappingWriter {
 						}
 					}
 
-					writtenClass = String.join("$", writtenClassParts);
+					lastWrittenClass = String.join("$", writtenClassParts);
 				} else {
-					writtenClass = "";
+					lastWrittenClass = "";
 					Files.createDirectories(file.getParent());
 				}
 
 				writer = Files.newBufferedWriter(file, StandardOpenOption.WRITE, StandardOpenOption.APPEND, StandardOpenOption.CREATE);
 			}
 
-			// write mismatched/missing class parts
-
-			indent = 0;
-			int srcStart = 0;
-
-			do {
-				int srcEnd = getNextOuterEnd(srcClassName, srcStart);
-				if (srcEnd < 0) srcEnd = srcClassName.length();
-				int srcLen = srcEnd - srcStart;
-
-				if (!writtenClass.regionMatches(srcStart, srcClassName, srcStart, srcLen) // writtenPart.startsWith(srcPart)
-						|| srcEnd < writtenClass.length() && writtenClass.charAt(srcEnd) != '$') { // no trailing characters in writtenPart -> startsWith = equals
-					writeIndent(0);
-					writer.write("CLASS ");
-					writer.write(srcClassName, srcStart, srcLen);
-
-					if (dstName != null) {
-						int dstStart = 0;
-
-						for (int i = 0; i < indent; i++) {
-							dstStart = getNextOuterEnd(dstName, dstStart);
-							if (dstStart < 0) break;
-							dstStart++;
-						}
-
-						if (dstStart >= 0) {
-							int dstEnd = getNextOuterEnd(dstName, dstStart);
-							if (dstEnd < 0) dstEnd = dstName.length();
-							int dstLen = dstEnd - dstStart;
-
-							if (dstLen != srcLen || !srcClassName.regionMatches(srcStart, dstName, dstStart, srcLen)) { // src != dst
-								writer.write(' ');
-								writer.write(dstName, dstStart, dstLen);
-							}
-						}
-					}
-
-					writer.write('\n');
-				}
-
-				indent++;
-				srcStart = srcEnd + 1;
-			} while (srcStart < srcClassName.length());
-
-			writtenClass = srcClassName;
-			dstName = null;
+			writeMismatchedOrMissingClasses();
 		} else if (targetKind == MappedElementKind.FIELD || targetKind == MappedElementKind.METHOD) {
 			writer.write(' ');
 			writer.write(desc);
@@ -259,73 +141,5 @@ public final class EnigmaDirWriter implements MappingWriter {
 		return true;
 	}
 
-	private static int getNextOuterEnd(String name, int startPos) {
-		int pos;
-
-		while ((pos = name.indexOf('$', startPos + 1)) > 0) {
-			if (name.charAt(pos - 1) != '/') return pos;
-			startPos = pos + 1;
-		}
-
-		return -1;
-	}
-
-	@Override
-	public void visitComment(MappedElementKind targetKind, String comment) throws IOException {
-		int start = 0;
-		int pos;
-
-		do {
-			if (start >= comment.length()) break;
-			pos = comment.indexOf('\n', start);
-			int end = pos >= 0 ? pos : comment.length();
-
-			writeIndent(targetKind.level);
-			writer.write("COMMENT");
-
-			if (end > start) {
-				writer.write(' ');
-
-				for (int i = start; i < end; i++) {
-					char c = comment.charAt(i);
-					int idx = toEscape.indexOf(c);
-
-					if (idx >= 0) {
-						if (i > start) writer.write(comment, start, i - start);
-						writer.write('\\');
-						writer.write(escaped.charAt(idx));
-						start = i + 1;
-					}
-				}
-
-				if (start < end) writer.write(comment, start, end - start);
-			}
-
-			writer.write('\n');
-
-			start = end + 1;
-		} while (pos >= 0);
-	}
-
-	private void writeIndent(int extra) throws IOException {
-		for (int i = 0; i < indent + extra; i++) {
-			writer.write('\t');
-		}
-	}
-
-	private static final Set<MappingFlag> flags = EnumSet.of(MappingFlag.NEEDS_UNIQUENESS, MappingFlag.NEEDS_SRC_FIELD_DESC, MappingFlag.NEEDS_SRC_METHOD_DESC);
-	private static final String toEscape = "\\\n\r\0\t";
-	private static final String escaped = "\\nr0t";
-
 	private final Path dir;
-
-	private Writer writer;
-	private String writerClass;
-	private String writtenClass;
-	private int indent;
-
-	private String srcClassName;
-	private String dstName;
-
-	private String desc;
 }
