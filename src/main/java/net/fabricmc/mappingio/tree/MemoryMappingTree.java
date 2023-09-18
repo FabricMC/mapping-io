061686394ae46e65c87a3ee19419ccd77caad3e0
NebelNidas <nebelnidas@gmail.com>
2023-09-18 13:17:34 +0200

    Remove unused imports

--- src/main/java/net/fabricmc/mappingio/tree/MemoryMappingTree.java
+++ src/main/java/net/fabricmc/mappingio/tree/MemoryMappingTree.java
@@ -17,20 +17,17 @@
 package net.fabricmc.mappingio.tree;
 
 import java.io.IOException;
-import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.IdentityHashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import net.fabricmc.mappingio.MappedElementKind;
 import net.fabricmc.mappingio.MappingFlag;
