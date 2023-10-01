6ea34bd67bba8ee2726cfe057d2ad523c77017cb
NebelNidas <nebelnidas@gmail.com>
2023-10-01 21:41:49 +0200

    Don't crash on empty TSRG files

--- src/main/java/net/fabricmc/mappingio/format/tsrg/TsrgFileReader.java
+++ src/main/java/net/fabricmc/mappingio/format/tsrg/TsrgFileReader.java
@@ -101,7 +101,11 @@ public final class TsrgFileReader {
 
 					String srcName = reader.nextCol();
 					if (srcName == null || srcName.endsWith("/")) continue;
-					if (srcName.isEmpty()) throw new IOException("missing class-name-a in line "+reader.getLineNumber());
+
+					if (srcName.isEmpty()) {
+						if (!reader.nextLine(0)) break; // Empty file
+						throw new IOException("missing class-name-a in line "+reader.getLineNumber());
+					}
 
 					if (visitor.visitClass(srcName)) {
 						readClass(reader, isTsrg2, dstNsCount, nameTmp, visitor);
