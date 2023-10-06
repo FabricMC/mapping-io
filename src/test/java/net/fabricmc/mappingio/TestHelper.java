2a4027168fe9548bbf14d4002bd0aec436ea8560
Julian Burner <48808497+NebelNidas@users.noreply.github.com>
2023-10-06 15:48:55 +0200

    Don't crash on empty TSRG files (#43)
    * Don't crash on empty TSRG files
    
    * Properly fix `ColumnFileReader` instead
    
    * Add SRG read test, SRG and TSRG to detection test

--- src/test/java/net/fabricmc/mappingio/TestHelper.java
+++ src/test/java/net/fabricmc/mappingio/TestHelper.java
@@ -20,9 +20,11 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 import net.fabricmc.mappingio.format.MappingFormat;
 import net.fabricmc.mappingio.tree.MappingTree;
+import net.fabricmc.mappingio.tree.MemoryMappingTree;
 
 public final class TestHelper {
 	public static Path getResource(String slashPrefixedResourcePath) {
@@ -37,5 +39,37 @@ public final class TestHelper {
 		MappingWriter writer = MappingWriter.create(dir.resolve(format.name() + "." + format.fileExt), format);
 		tree.accept(writer);
 		writer.close();
+	}
+
+	// Has to be kept in sync with /resources/read/valid/* test mappings!
+	public static MemoryMappingTree createTestTree() {
+		MemoryMappingTree tree = new MemoryMappingTree();
+		tree.visitNamespaces(MappingUtil.NS_SOURCE_FALLBACK, Arrays.asList(MappingUtil.NS_TARGET_FALLBACK));
+
+		tree.visitClass("class_1");
+		tree.visitDstName(MappedElementKind.CLASS, 0, "RenamedClass1");
+
+		tree.visitField("field_1", "I");
+		tree.visitDstName(MappedElementKind.FIELD, 0, "renamedField");
+
+		tree.visitMethod("method_1", "(F)I");
+		tree.visitDstName(MappedElementKind.METHOD, 0, "renamedMethod");
+
+		tree.visitMethodArg(0, 0, "param_1");
+		tree.visitDstName(MappedElementKind.METHOD_ARG, 0, "renamedParameter");
+
+		tree.visitMethodVar(0, 0, 0, 0, "param_1");
+		tree.visitDstName(MappedElementKind.METHOD_VAR, 0, "renamedVariable");
+
+		tree.visitClass("class_1$class_2");
+		tree.visitDstName(MappedElementKind.CLASS, 0, "RenamedClass1$RenamedInnerClass");
+
+		tree.visitField("field_1", "I");
+		tree.visitDstName(MappedElementKind.FIELD, 0, "renamedField2");
+
+		tree.visitClass("class_3");
+		tree.visitDstName(MappedElementKind.CLASS, 0, "RenamedClass2");
+
+		return tree;
 	}
 }
