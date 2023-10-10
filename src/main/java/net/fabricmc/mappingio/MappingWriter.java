d777424edcd284212e24cce3dcf02ab1518d01ff
NebelNidas <nebelnidas@gmail.com>
2023-10-10 20:22:08 +0200

    Make `MappingWriter#create` return null instead of throwing an exception

--- src/main/java/net/fabricmc/mappingio/MappingWriter.java
+++ src/main/java/net/fabricmc/mappingio/MappingWriter.java
@@ -22,6 +22,8 @@ import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.jetbrains.annotations.Nullable;
+
 import net.fabricmc.mappingio.format.MappingFormat;
 import net.fabricmc.mappingio.format.enigma.EnigmaDirWriter;
 import net.fabricmc.mappingio.format.enigma.EnigmaFileWriter;
@@ -30,17 +32,19 @@ import net.fabricmc.mappingio.format.tiny.Tiny1FileWriter;
 import net.fabricmc.mappingio.format.tiny.Tiny2FileWriter;
 
 public interface MappingWriter extends Closeable, MappingVisitor {
+	@Nullable
 	static MappingWriter create(Path file, MappingFormat format) throws IOException {
 		if (format.hasSingleFile()) {
 			return create(Files.newBufferedWriter(file), format);
 		} else {
 			switch (format) {
 			case ENIGMA_DIR: return new EnigmaDirWriter(file, true);
-			default: throw new UnsupportedOperationException("format "+format+" is not implemented");
+			default: return null;
 			}
 		}
 	}
 
+	@Nullable
 	static MappingWriter create(Writer writer, MappingFormat format) throws IOException {
 		if (!format.hasSingleFile()) throw new IllegalArgumentException("format "+format+" is not applicable to a single writer");
 
@@ -49,7 +53,7 @@ public interface MappingWriter extends Closeable, MappingVisitor {
 		case TINY_2_FILE: return new Tiny2FileWriter(writer, false);
 		case ENIGMA_FILE: return new EnigmaFileWriter(writer);
 		case PROGUARD_FILE: return new ProGuardFileWriter(writer);
-		default: throw new UnsupportedOperationException("format "+format+" is not implemented");
+		default: return null;
 		}
 	}
 
