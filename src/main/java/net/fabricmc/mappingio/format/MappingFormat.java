e30d050ffdc22dcf7b567a515e4cde3accea001a
NebelNidas <nebelnidas@gmail.com>
2024-04-12 17:35:57 +0200

    Revert "Rename `ElementCommentSupport#SHARED` to `SINGLE`"
    This reverts commit 046ca1fbb0d77345e00e168759f9571793c8121d.

--- src/main/java/net/fabricmc/mappingio/format/MappingFormat.java
+++ src/main/java/net/fabricmc/mappingio/format/MappingFormat.java
@@ -159,7 +159,7 @@ public enum MappingFormat {
 					.withStartOpIndices(OptionalFeature.REQUIRED)
 					.withSrcNames(OptionalFeature.OPTIONAL)
 					.withDstNames(OptionalFeature.OPTIONAL))
-			.withElementComments(ElementCommentSupport.SINGLE)
+			.withElementComments(ElementCommentSupport.SHARED)
 			.withFileComments()), // not sure about this one
 
 	/**
@@ -181,7 +181,7 @@ public enum MappingFormat {
 			.withArgs(a -> a
 					.withLvIndices(OptionalFeature.REQUIRED)
 					.withDstNames(OptionalFeature.OPTIONAL))
-			.withElementComments(ElementCommentSupport.SINGLE)
+			.withElementComments(ElementCommentSupport.SHARED)
 			.withFileComments()),
 
 	/**
@@ -804,7 +804,7 @@ public enum MappingFormat {
 
 		public enum ElementCommentSupport {
 			NAMESPACED,
-			SINGLE,
+			SHARED,
 			NONE
 		}
 	}
