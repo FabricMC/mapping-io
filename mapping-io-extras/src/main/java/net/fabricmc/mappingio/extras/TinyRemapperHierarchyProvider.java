fac6a19e753227d484b0dd3b4ceb3a354001c6f9
NebelNidas <nebelnidas@gmail.com>
2025-04-05 09:07:52 +0200

    Tiny Remapper does support null descriptors if configured appropriately

--- mapping-io-extras/src/main/java/net/fabricmc/mappingio/extras/TinyRemapperHierarchyProvider.java
+++ mapping-io-extras/src/main/java/net/fabricmc/mappingio/extras/TinyRemapperHierarchyProvider.java
@@ -64,8 +64,6 @@ public final class TinyRemapperHierarchyProvider implements HierarchyInfoProvide
 	@Override
 	@Nullable
 	public String resolveMethod(String owner, String name, @Nullable String desc) {
-		if (desc == null) return null; // TODO: Tiny Remapper limitation
-
 		TrClass cls = env.getClass(owner);
 		if (cls == null) return null;
 
@@ -77,8 +75,6 @@ public final class TinyRemapperHierarchyProvider implements HierarchyInfoProvide
 	@Override
 	@Nullable
 	public HierarchyData getMethodHierarchy(String owner, String name, @Nullable String desc) {
-		if (desc == null) return null; // TODO: Tiny Remapper limitation
-
 		TrClass cls = env.getClass(owner);
 		if (cls == null) return null;
 
